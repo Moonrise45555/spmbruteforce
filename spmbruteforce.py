13c6b0d4fa8724790774b21e51cb7f4dff8f6fef
Moonrise45555 <j.kerz@gmx.net>
2024-02-08 19:02:03 +0100

    total rework, now works up to 16 squigs in a realistic timeframe. i wonder how far i can push this?

--- spmbruteforce.py
+++ spmbruteforce.py
@@ -15,53 +15,48 @@ def SimuQuickEnd(crntsquiglet,crntpoints,crntChain):
 def BruteForce(crntEnemies,crntPoints,crntChain,depth=0,prnt=False):
     global maxpnts
     crntAttack = (crntPoints > 20000) + 1 + (crntPoints > 60000)
-   
     if crntEnemies == []:
         if crntPoints > maxpnts:
             maxpnts = crntPoints
         return
-    if (crntAttack >= max(crntEnemies)):
-        #all enemies will go down in one hit anyway: it doesnt matter which one we choose
-        SimuQuickEnd(crntEnemies,crntPoints,crntChain)
-    else:
-        for i in range(len(crntEnemies)):
-            seenHPs = []
-            HPindex = []
-            if crntEnemies[i] in seenHPs:
-                continue
-            else:
-                seenHPs.append(crntEnemies[i])
-                HPindex.append(i)
+    seenHPs = []
+    HPindex = []
+    for i in range(len(crntEnemies)):
+        
+        
+        if seenHPs.count(crntEnemies[i]) > 0:
+            continue
+        else:
+            seenHPs.append(crntEnemies[i])
+            HPindex.append(i)
 
 
 
 
 
 
-
-        for i in range(len(seenHPs)):
-            sq = copy(crntEnemies)
-            sq[HPindex[i]] -= crntAttack #applies the damage
-            if sq[HPindex[i]] <= 0:
-                newChain = crntChain + 100
-                del sq[HPindex[i]]
-            else:
-                newChain = crntChain
-            newpnts = crntPoints + newChain
-            
-
-            BruteForce(sq,newpnts,newChain,depth + 1)
+    for i in HPindex:
+           sq = copy(crntEnemies)
+           sq[i]-= crntAttack #applies the damage
+           if sq[i] <= 0:
+               newChain = crntChain + 100
+               del sq[i]
+           else:
+               newChain = crntChain
+           newpnts = crntPoints + newChain
+           
+           BruteForce(sq,newpnts,newChain,depth + 1)
     
 
 
 
-for i in range(22):
-    print(i)
+for i in range(1000):
+    print("# of squigs ",i)
     t = perf_counter()
     BruteForce([2] * i,0,0)
-    print(maxpnts)
+    print("max points  ", maxpnts)
     t = perf_counter() - t
-    print(t)
+    print("time taken  ",t)
 
 #BruteForce(deepcopy(squiglets),0,0)
 
